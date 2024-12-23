9380fd0ac85e9ab27c97ef176dda3998e9492f80
Douglas Tawile <douglastawile@gmail.com>
2024-12-23 03:55:56 -0500

    Setting up the kings height realty project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/services" element={<h1>Services</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin"), flowbite.plugin()],
+  darkMode: "selector",
+};
